a0bd7d28c59c60c15007d3289f6f9ec410ad1355
Sh786-cloud <160265285+Sh786-cloud@users.noreply.github.com>
2024-03-29 09:07:58 +0500

    added a welcome.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 import inquirer from "inquirer"
 
+console.log("Welcome to Number Guessing Game")
 const randomNumber =  Math.floor(Math.random() * 6 + 1)
 const answer = await inquirer.prompt([
     {
